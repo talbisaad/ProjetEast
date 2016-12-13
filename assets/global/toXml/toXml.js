505c648bd29f5acd2ae2c6e31f5f644faf527094
Saad TALBI <talbisaad_007@hotmail.fr>
2016-12-13 10:43:06 +0100

    from view to xml first method

---
+++ assets/global/toXml/toXml.js
@@ -0,0 +1,41 @@
+ var handleTabs = function() {
+        //activate tab if tab id provided in the URL
+        if (encodeURI(location.hash)) {
+            var tabid = encodeURI(location.hash.substr(1));
+            $('a[href="#' + tabid + '"]').parents('.tab-pane:hidden').each(function() {
+                var tabid = $(this).attr("id");
+                $('a[href="#' + tabid + '"]').click();
+            });
+            $('a[href="#' + tabid + '"]').click();
+        }
+
+        if ($().tabdrop) {
+            $('.tabbable-tabdrop .nav-pills, .tabbable-tabdrop .nav-tabs').tabdrop({
+                text: '<i class="fa fa-ellipsis-v"></i>&nbsp;<i class="fa fa-angle-down"></i>'
+            });
+        }
+    };
+
+    var handleiCheck = function() {
+        if (!$().iCheck) {
+            return;
+        }
+
+        $('.icheck').each(function() {
+            var checkboxClass = $(this).attr('data-checkbox') ? $(this).attr('data-checkbox') : 'icheckbox_minimal-grey';
+            var radioClass = $(this).attr('data-radio') ? $(this).attr('data-radio') : 'iradio_minimal-grey';
+
+            if (checkboxClass.indexOf('_line') > -1 || radioClass.indexOf('_line') > -1) {
+                $(this).iCheck({
+                    checkboxClass: checkboxClass,
+                    radioClass: radioClass,
+                    insert: '<div class="icheck_line-icon"></div>' + $(this).attr("data-label")
+                });
+            } else {
+                $(this).iCheck({
+                    checkboxClass: checkboxClass,
+                    radioClass: radioClass
+                });
+            }
+        });
+    };
